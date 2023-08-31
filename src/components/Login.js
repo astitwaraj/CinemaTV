eed914bb3c9db7e3fb645f1fe68f236f43a67cf6
Astitwa Raj <astitva007raj@gmail.com>
2023-09-01 00:41:09 +0530

    Form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { LOGIN_BG } from "../utils/ImageLinks";
+import { checkValid } from "../utils/formValidation";
 import Header from "./Header";
 
 const Login = () => {
   const [signInstate, setsignInState] = useState(true);
+  const [errorMessage, seterrorMessage] = useState(null);
   const toggleForm = () => {
     setsignInState(!signInstate);
+  };
+  const email = useRef(null);
+  const password = useRef(null);
+  const handlelogin = () => {
+    seterrorMessage(checkValid(email.current.value, password.current.value));
   };
   return (
     <div>
       <Header />
       <div className="absolute">
-        <img className="md:max-w-screen-2xl" src={LOGIN_BG} alt="" />
+        <img className="" src={LOGIN_BG} alt="" />
       </div>
       <div className="flex place-content-center">
-        <form className="absolute bg-black text-white w-3/12 top-24 h-3/4 rounded-md bg-opacity-80 flex flex-col">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="absolute bg-black text-white w-3/12 top-24 h-3/4 rounded-md bg-opacity-80 flex flex-col"
+        >
           <h1 className="font-bold text-3xl ml-10 mt-16 mb-5">
             {signInstate ? "Sign In" : "Sign Up"}
           </h1>
@@ -26,16 +36,31 @@ const Login = () => {
             />
           )}
           <input
+            ref={email}
             type="email"
             placeholder="Email Address"
             className="p-3 my-5 mx-10 rounded-md bg-zinc-700 focus:outline-none"
           />
+          {errorMessage === "Email is not valid" && (
+            <span className="text-red-600 text-xs mx-10 -mt-5">
+              {errorMessage}
+            </span>
+          )}
           <input
+            ref={password}
             type="password"
             placeholder="Password"
             className="p-3 my-5 mx-10 rounded-md  bg-zinc-700 focus:outline-none"
           />
-          <button className="bg-red-600 text-white p-3 mx-10 my-5 font-semibold rounded-md ">
+          {errorMessage === "Password is not valid" && (
+            <span className="text-red-600 text-xs -mt-5 mx-10 ">
+              {errorMessage}
+            </span>
+          )}
+          <button
+            onClick={handlelogin}
+            className="bg-red-600 text-white p-3 mx-10 my-5 font-semibold rounded-md "
+          >
             {signInstate ? "Sign In" : "Sign Up"}
           </button>
 
---
+++ src/utils/formValidation.js
@@ -0,0 +1,11 @@
+export const checkValid = (email, password) => {
+  const emailCheck = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+  const passwordCheck =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  if (!emailCheck) return "Email is not valid";
+  if (!passwordCheck) return "Password is not valid";
+  return null;
+};
